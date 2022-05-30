7188589ac91e615f60cd88ad401ce5b815f26f0e
Leonardo Kasperavičius <leocck@gmail.com>
2022-05-30 20:08:33 +0200

    Restructure project organisation

--- src/main/java/options/Register.java
+++ src/main/java/options/Register.java
@@ -1,4 +1,4 @@
-package data;
+package options;
 import java.util.Scanner;
 
 
---
+++ src/main/java/options/Service.java
@@ -0,0 +1,57 @@
+package options;
+
+import java.util.Scanner;
+
+public class Service {
+
+//    Scanner input = new Scanner(System.in);
+//
+//    public void printMenu(User u){
+//        System.out.println("1. Deposit Money");
+//        System.out.println("2. Withdraw Money");
+//        System.out.println("3. Show Bank Statement");
+//        System.out.println("4. Transfer to another user");
+//        System.out.println("5. Search by reference");
+//        System.out.println("6. Change your password");
+//
+//
+//        if(u.username == "Admin"){
+//            System.out.println("9. Show all users");
+//        }
+//        System.out.println("0. exit");
+//
+//    }
+//
+//    public String askService(){
+//        return this.input.nextLine();
+//    }
+//
+//    public String askReference(){
+//
+//        System.out.println("please write your reference type");
+//        System.out.println("1.Gift");
+//        System.out.println("2.Family Assistance");
+//        System.out.println("3.Hobbies");
+//        System.out.println("4.Paying Bills");
+//
+//        String referenceType = input.nextLine();
+//        switch(referenceType){
+//            case "1":
+//                return "Gift";
+//
+//            case "2":
+//                return "Family";
+//
+//            case "3":
+//                return "Hobbies";
+//
+//            case "4":
+//                return "Gift";
+//
+//            default:
+//                return " no reference";
+//        }
+//
+//
+//    }
+}
